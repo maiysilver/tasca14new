2397bc70b5ed64bdae36406e4954c8b69b96d47e
maiysilver <48027427+maiysilver@users.noreply.github.com>
2019-03-11 17:37:21 +0100

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,5 +1,5 @@
 // fichero xml que está en el servidor rawgit
-var url="https://rawgit.com/JuanAntonioBieto/LM-Formulario/master/xml/xmlconDTD.xml";
+var url="xml/preguntas.xml";
 
 //Variables
 var i = 0;
